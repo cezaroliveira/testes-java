49e04b426e2b86440027bf9dd498ae41b865e18d
cezaroliveira <cezaroliveira1987@gmail.com>
2021-06-29 20:29:18 -0300

    Outro exemplo de NullPointerException em tempo de execução.

--- src/TesteMain.java
+++ src/TesteMain.java
@@ -3,15 +3,22 @@ public class TesteMain {
 
 	public static void main(String[] args) {
 
+		System.out.println("Início do método main");
+
 		testarErroCompilacao();
 
 		testarErroTempoExecucao();
+
+		testarErroTempoExecucaoV2(args);
+
+		System.out.println("Fim do método main");
 	}
 
 	private static void testarErroCompilacao() {
 
-		// Definido como Integer, mas recebendo uma String, ou seja, tipos incompatíveis.
-		Integer numero = "a";
+		// Definido como Integer, mas recebendo uma String, ou seja, tipos
+		// incompatíveis.
+//		Integer numero = "a";
 	}
 
 	private static void testarErroTempoExecucao() {
@@ -26,6 +33,27 @@ public class TesteMain {
 		 * indicar um possível problema em tempo de execução.
 		 * 
 		 * Será lançado NullPointerException em tempo de execução.
+		 */
+		palavra.concat("outra palavra");
+	}
+
+	private static void testarErroTempoExecucaoV2(String[] args) {
+
+		// Definido como null, ou seja, sem referência.
+		String palavra = null;
+
+		if (args != null) {
+			palavra = "args foi informado";
+		}
+
+		/*
+		 * Tentando acionar um método em uma referência que pode ser nula.
+		 * 
+		 * Não existe problema de compilação, porém a IDE tem ferramentas que podem
+		 * indicar um possível problema em tempo de execução.
+		 * 
+		 * Será lançado NullPointerException em tempo de execução caso não seja
+		 * inicializada corretamente.
 		 */
 		palavra.concat("outra palavra");
 	}
